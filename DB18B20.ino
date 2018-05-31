f69740a009d4b0b488ab2b2c9f219a9f6bcfddc8
adridelahoz <36639308+adridelahoz@users.noreply.github.com>
2018-05-31 15:57:44 +0200

    Create DB18B20.ino

---
+++ DB18B20.ino
@@ -0,0 +1,27 @@
+#include <OneWire.h>
+#incclude <DallasTemperature.h>
+
+//pin donde se conecta el bus 1-Wire
+const int pinDatosDQ=9;
+
+//Instancia a las clases de OneWire y DallasTemperature
+OneWire oneWireObjeto(pinDatosDQ);
+DallasTemperature sensorDS18B20(&oneWireObjeto);
+void setup(){
+  //Iniciamos la comunicacion serie
+  Serial.begin(9600);
+  //Iniciamos el bus 1-Wire
+  sensorDS18B20.begin();
+}
+void loop(){
+  //Mandamos comandos para la toma de temperatura a los sensores
+  Serial.println("Mandando comandos a los sensores");
+  sensorDS18B20.requestTemperatures();
+  
+  //Leemos y mostramos los datos de los sensores DS18B20
+  Serial.print(Temperatura sensor: );
+  Serial.print(sensorDS18B20.getTempCByIndex());
+  Serial.println(" C");
+
+  delay(1000); //esperamos 1 segundo para la siguente lectura
+}
