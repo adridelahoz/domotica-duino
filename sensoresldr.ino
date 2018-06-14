ee2806f1064f2409a272554409f38a683273e01a
adridelahoz <36639308+adridelahoz@users.noreply.github.com>
2018-06-14 16:45:50 +0200

    Update sensoresldr.ino

--- sensoresldr.ino
+++ sensoresldr.ino
@@ -1,51 +1,108 @@
 #include <OneWire.h>                 //Se importan las librerías
 #include <DallasTemperature.h>
-#define Pin 2                        //Se declara el pin donde se conectará la DATA
+#include <DHT.h>
+ 
+// Definimos el pin digital donde se conecta el sensor
+#define DHTPIN 2
+// Dependiendo del tipo de sensor
+#define DHTTYPE DHT11
+ 
+// Inicializamos el sensor DHT11
+
+#define Pin 4                      //Se declara el pin donde se conectará la DATA
 OneWire ourWire(Pin);                //Se establece el pin declarado como bus para la comunicación OneWire
 DallasTemperature sensors(&ourWire); //Se llama a la librería DallasTemperature
-
+DHT dht(DHTPIN, DHTTYPE);
 const char depu = 4;
 const char temp = 3;
 const int hume = A0;
+const int lluv = A2;
 const int motor = 7;
-const int LDRpin = A1;
-int valorHumedad;
-int tempera;
-int luz;
+const int pinLDR = A1;
 volatile int NbTopsFan;
 int Calc;
 int caudal = 1;
-int t=5000;
 
 
 void setup () {
   Serial.begin(9600);//Inicializo el puerto serial a 9600 baudios
-    pinMode(hume, INPUT);
-   	pinMode(temp, INPUT);
+        pinMode(hume, INPUT);
+        pinMode(lluv, INPUT);
+   		pinMode(temp, INPUT);
 		pinMode(motor, OUTPUT);
-		pinMode(LDRpin, INPUT);
+		pinMode(pinLDR, INPUT);
         sensors.begin();
         pinMode(caudal, INPUT);
         attachInterrupt(0, rpm, RISING);
+		dht.begin();
 
 }
 void loop () {
   
   if (Serial.available()>0) { //Si hay comunicacion serial
-  char c = Serial.read(); //Guardamos la lectura en una variable 
-  humedad();
-  temperatura();
-  calccaudal();
-  int valldr=digitalRead(LDRpin);
-	if ((valorHumedad>50)||tempera>32||luz==HIGH){
-		digitalWrite(motor, 0);
-		Serial.println ("Motor apagado");
-	}
-	else {
-		digitalWrite(motor, 1);	
-		Serial.println ("Motor Encendido");
-	}
-  delay(500);
+
+    Serial.println(" ");
+    Serial.println(" ");
+    Serial.println(" ");
+  sensors.requestTemperatures();       //Prepara el sensor para la lectura
+  int tempera = (sensors.getTempCByIndex(0)); //Se lee e imprime la temperatura en grados Centigrados
+  Serial.print(tempera);
+  Serial.println(" Grados Centigrados");
+
+       //Se hace la lectura analoga del pin A0 (sensor) y se pasa por la funcion
+      //map() para ajustar los valores leidos a los porcentajes que queremos utilizar		
+	//int valorHumedad = map(analogRead(humedad), 0, 1023, 100, 0);	
+	Serial.print("Humedad tierra: "); //Se lee e imprime la humedad en %
+	Serial.print(map(analogRead(hume), 0, 1023, 100, 0));
+	Serial.println("%");
+  
+
+       //Se hace la lectura analoga del pin A2 (sensor) y se pasa por la funcion
+      //map() para ajustar los valores leidos a los porcentajes que queremos utilizar    
+  //int valorlluvia = map(analogRead(lluvia), 0, 1023, 100, 0); 
+  Serial.print("Lluvia: "); //Se lee e imprime la humedad en %
+  Serial.print(map(analogRead(lluv), 0, 1023, 100, 0));
+  Serial.println("%");
+	  
+	  
+	  
+	float h = dht.readHumidity();
+  // Leemos la temperatura en grados centígrados (por defecto)
+  float t = dht.readTemperature();
+  // Leemos la temperatura en grados Fahrenheit
+  float f = dht.readTemperature(true);
+ 
+  // Comprobamos si ha habido algún error en la lectura
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println("Error obteniendo los datos del sensor DHT11");
+  }
+ 
+  // Calcular el índice de calor en Fahrenheit
+  float hif = dht.computeHeatIndex(f, h);
+  // Calcular el índice de calor en grados centígrados
+  float hic = dht.computeHeatIndex(t, h, false);
+ 
+  Serial.print("Humedad: ");
+  Serial.print(h);
+  Serial.println(" %\t");
+  Serial.print("Temperatura: ");
+  Serial.print(t);
+  Serial.println(" *C ");
+
+
+
+   // Guardamos el valor leido del ADC en una variable
+  // El valor leido por el ADC (voltaje) aumenta de manera directamente proporcional
+  // con respecto a la luz percibida por el LDR
+  int valorLDR= analogRead(pinLDR);
+  
+  // Devolver el valor leido a nuestro monitor serial en el IDE de Arduino
+if (valorLDR>300){
+  Serial.println ("Es de noche.");
+  
+  }else Serial.println("Es de dia.");
+	  
+  delay(10000);
   }
 }
 
@@ -53,33 +110,12 @@ void rpm ()
 {
 NbTopsFan++;
 }
-
-void temperatura(){
-        sensors.requestTemperatures();       //Prepara el sensor para la lectura
- 
-        tempera = (sensors.getTempCByIndex(0)); //Se lee e imprime la temperatura en grados Centigrados
-		Serial.print(tempera);
-        Serial.println(" Grados Centigrados");
-}
 void calccaudal(){
         NbTopsFan = 0;
         sei();
-        
         cli();
         Calc = (NbTopsFan * 60 / 5.5);
         Serial.print (Calc, DEC);
         Serial.println (" Litros/min\r\n");
 }
 
-
-void humedad(){
-  int humedad = analogRead(hume);
-  Serial.print(humedad);
-      //Se hace la lectura analoga del pin A0 (sensor) y se pasa por la funcion
-      //map() para ajustar los valores leidos a los porcentajes que queremos utilizar		
-	int valorHumedad = map(analogRead(humedad), 0, 1023, 100, 0);	
-	Serial.print("Humedad: "); //Se lee e imprime la humedad en %
-	Serial.print(valorHumedad);
-	Serial.println("%");
-	return (valorHumedad);
-}
